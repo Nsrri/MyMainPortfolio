0f16540083de9471b5ed64f5433213ebe6324326
Nsrri <nasrin.jafari@powercoders.orgr>
2024-04-16 05:41:18 +0200

    change the place of functions

--- public/src/scripts.js
+++ public/src/scripts.js
@@ -38,9 +38,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // This is part make the project creation dynamic.
 const params = new URLSearchParams(window.location.search);
-const description = params.get("description");
-const image = params.get("img");
-const title = params.get("title");
+const description = params.get('description');
+const image = params.get('img');
+const title = params.get('title');
 
 const descriptionElement = document.getElementById("description");
 const titleElement = document.getElementById("title");
@@ -49,6 +49,7 @@ const imageElement = document.getElementById("img-detail");
 descriptionElement.textContent = description;
 titleElement.textContent = title;
 imageElement.src = image;
+
 
 function openDetail(message, title, image) {
   window.open(
@@ -62,32 +63,7 @@ function openDetail(message, title, image) {
   );
 }
 
-document
-  .getElementById("sendMessageBtn")
-  .addEventListener("click", function () {
-    var contactName = document.getElementById("name").value;
-    var contactEmail = document.getElementById("email").value;
-    var contactSubject = document.getElementById("subject").value;
-    var contactMessage = document.getElementById("message").value;
 
-    axios
-      .post("/.netlify/functions/send-contact-email", {
-        name: contactName,
-        email: contactEmail,
-        subject: contactSubject,
-        message: contactMessage,
-      })
-      .then(function (response) {
-        document.getElementById("messageResponse").innerHTML =
-          '<p class="success-message">' + response.data + "</p>";
-        resetForm();
-      })
-      .catch(function (error) {
-        document.getElementById("messageResponse").innerHTML =
-          '<p class="error-message">' + error.response.data + "</p>";
-      });
-  });
 
-function resetForm() {
-  document.getElementById("contactForm").reset();
-}
+
+
